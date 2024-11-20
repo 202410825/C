d849fc20357938c6b2278ef86f08708450f4416a
unknown <cutemj127@gmail.com>
2024-11-20 22:50:29 +0900

    Add Text File [hw8.c]

---
+++ hw8.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+double pow(double* a)
+{
+	return pow(a, 2);
+}
+double sqrt(double x)
+{
+	return sqrt(x);
+}
+int main(void)
+{
+	int* arr[5];
+	double total = 0;
+	for (int i = 0; i < 5; i++) {
+		scamf("%d", arr[i]);
+	}
+	for (int i = 0; i < 5; i++) {
+		total += 
+	}
+
+
+	return 0;
+}
+
+
